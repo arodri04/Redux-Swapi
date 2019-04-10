fd349aaae109a9a89d7f7a6c74cc204eb17098bd
arodri04 <augustine.rodriguez55@gmail.com>
2019-04-10 16:36:52 -0600

    mvp

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,24 @@
 // we'll need axios
-
+import axios from "axios";
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
-
+export const FETCHING = "FETCHING";
+export const FAILURE = "FAILURE";
+export const SUCCESS = "SUCCESS";
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+export const Characters = () => dispatch => {
+  axios
+    .get("https://swapi.co/api/people/")
+    .then(({ data }) => {
+      console.log(data.results);
+      dispatch({
+        type: SUCCESS,
+        payload: data.results
+      });
+    })
+    .catch(error => {
+      dispatch({ type: FAILURE, payload: error });
+    });
+};
--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import charsReducer from "./reducers";
 // needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+// applyMiddleware from redux*
+// thunk from redux-thunk*
+// logger from redux-logger*
+// rootReducer from ./reducers*
 
 const store = createStore(
-  rootReducer
+  charsReducer,
+  applyMiddleware(thunk, logger)
+
   /* applyMiddleware goes here */
 );
 
